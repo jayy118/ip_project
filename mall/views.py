82f92a7572e0fbf0b1767d6d0b6da93b98f1ec2f
=Jae-Hyun Lee <hyun918@duksung.ac.kr>
2021-12-03 20:41:33 +0900

    Products 목록페이지 만들기

--- mall/views.py
+++ mall/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from mall.models import Product
+
+
+def index(request):
+    products = Product.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'mall/index.html',
+        {
+            'products': products,
+        }
+    )
