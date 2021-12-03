87edb605ab245c6a7190e0e482db8a4146a2b5de
=Jae-Hyun Lee <hyun918@duksung.ac.kr>
2021-12-04 02:07:47 +0900

    포스트 목록 페이지와 상세 페이지 디자인 통일하기

--- mall/models.py
+++ mall/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 
 # Create your models here.
@@ -12,12 +11,13 @@ class Product(models.Model):
     publisher = models.CharField(max_length=50)
 
     released_at = models.DateField(null=True)
-    head_image = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True) #-> 프로젝트 파일 > urls setting, static import urlpatterns 작성
+    head_image = models.ImageField(upload_to='mall/images/%Y/%m/%d/',
+                                   blank=True)  # -> 프로젝트 파일 > urls setting, static import urlpatterns 작성
 
-    #author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
-    #category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    # author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    # category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
-    #tags = models.ManyToManyField(Tag, blank=True)
+    # tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}] {self.name} :: {self.publisher}'
--- mall/tests.py
+++ mall/tests.py
@@ -1,3 +1,81 @@
-from django.test import TestCase
+from bs4 import BeautifulSoup
+from django.test import TestCase, Client
+from .models import Product
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        # 상품 목록 페이지를 가져온다
+        response = self.client.get('/mall/')
+        # 정상적으로 페이지가 로드
+        self.assertEqual(response.status_code, 200)
+        # 페이지 타이틀 'Mall'
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Mall')
+        # 네비게이션바가 있다
+        navbar = soup.nav
+        # 네비게이션바에 Mall, About Us 라는 문구가 있다
+        self.assertIn('Mall', navbar.text)
+        self.assertIn('About Us', navbar.text)
+
+        # 포스트(게시물)이 하나도 없는 경우
+        self.assertEqual(Product.objects.count(), 0)
+        # 적절한 안내 문구가 포함되어 있는지
+        main_area = soup.find('div', id="main-area")
+        self.assertIn('아직 상품이 없습니다.', main_area.text)
+
+        # 포스트(게시물)이 2개 존재하는 경우
+        product_001 = Product.objects.create(
+            name = '첫 번째 상품입니다.',
+            content = "Hello World!!! We are the world...",
+            price = 1234,
+            publisher = "abc"
+        )
+        product_002 = Product.objects.create(
+            name='두 번째 상품입니다.',
+            content="1등이 전부가 아니잖아요",
+            price = 1234,
+            publisher = "abc",
+        )
+        self.assertEqual(Product.objects.count(), 2)
+        # 목록페이지를 새롭게 불러와서
+        response = self.client.get('/mall/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # 상품(게시물)의 타이틀이 2개 존재하는가
+        main_area = soup.find('div', id="main-area")
+        self.assertIn(product_001.name, main_area.text)
+        self.assertIn(product_002.name, main_area.text)
+        self.assertNotIn('아직 상품이 없습니다.', main_area.text)
+
+    def test_post_detail(self):
+        # 상품 하나
+        product_001 = Product.objects.create(
+            name='첫 번째 상품입니다.',
+            content="Hello World!!! We are the world...",
+            price=1234,
+            publisher="abc"
+        )
+        # 이 상품의 url이 /blog/1
+        self.assertEqual(product_001.get_absolute_url(), '/mall/1/')
+        # url에 의해 정상적으로 상세페이지를 불러오는가
+        response = self.client.get('/mall/1/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # 상품목록과 같은 네비게이션바가 있는가
+        navbar = soup.nav
+        self.assertIn('Mall', navbar.text)
+        self.assertIn('About Us', navbar.text)
+        # 상품 name은 웹브라우저의 name에 있는가
+        self.assertIn(product_001.name, soup.title.text)
+        # 상품의 name은 포스트영역에도 있는가
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(product_001.name, post_area.text)
+        # 포스트 작성자가 있는가
+        # 아직 작성중
+        # 포스트 내용이 있는가
+        self.assertIn(product_001.content, post_area.text)
